8492cc9547d77b4b7fdbfd4c38a840ecab763673
Mitesh <miteshbediya1999@gmail.com>
2021-06-24 12:03:59 +0530

    init

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import {BrowserRouter as Router, Route} from "react-router-dom"
 import Home from "./pages/Home"
+import Spons from '../../src/components/pages/spons'
 
 interface Props {
     
@@ -10,6 +11,7 @@ const AppRoutes = (props: Props) => {
     return (
         <Router>
             <Route exact path="/" component={Home} />
+            <Route exact path="/spons" component={Spons}/>
         </Router>
     )
 }
