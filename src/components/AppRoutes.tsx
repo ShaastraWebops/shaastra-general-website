fa99a0b9a82543041d58be561fb0fbebfb188567
Rohit Bhagat <rohitbhagat01010@gmail.com>
2021-06-24 19:56:16 +0530

    Created the Team page

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -1,17 +1,17 @@
-import * as React from "react"
-import {BrowserRouter as Router, Route} from "react-router-dom"
-import Home from "./pages/Home"
+import * as React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Team from "./pages/Team_page/Team";
 
-interface Props {
-    
-}
+interface Props {}
 
 const AppRoutes = (props: Props) => {
-    return (
-        <Router>
-            <Route exact path="/" component={Home} />
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/team" component={Team} />
+    </Router>
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
