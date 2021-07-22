06186aca025bf2df6683fe90599a0788d6a74f5d
Mitesh <miteshbediya1999@gmail.com>
2021-07-22 18:01:14 +0530

    updated

--- src/components/pages/spons.tsx
+++ src/components/pages/spons.tsx
@@ -22,31 +22,32 @@ import Header from '../shared/Header';
 import Footer from '../shared/Footer';
 import CustomBox from '../shared/CustomBox';
 import catl from '../../images/logo/static/caterpillar22.png'
-
-var script = document.createElement('script');
-
-script.src = '//code.jquery.com/jquery-1.11.0.min.js';
-document.getElementsByTagName('head')[0].appendChild(script);
+import www from '../../images/logo/static/www.png'
+import linkedin from '../../images/logo/static/linkedin (1).png'
+import facebook from '../../images/logo/static/facebook.png';
 
 function Spons() {
 
     return (
         <>
             <CustomBox>
-
-                <Header /><meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                <Header /><meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <br />
                 <br />
                 <br />
                 <br />
                 <br />
+                {/* <h1 id="title1">Our Trustable Sponsors</h1> */}
+                <span id="titlemain">Shaastra Sponsors</span>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
                 <div className="main-boxx">
                     <div className="card__collection clear-fix mitesh">
                         <div className="cards cards--three" id="photo">
                             <img src={Astra} className="img-responsive" alt="" />
                             <span className="cards--three__rect-1">
                                 <span className="shadow-1"></span>
-                                <p className="text">AstraZeneca</p>
+                                <p className="text"><a href="https://www.astrazeneca.com/"><img src={www} id="www1" alt=""></img></a><a href="https://www.linkedin.com/company/astrazeneca/"><img alt="" src={linkedin} id="www2"></img></a><a href="https://www.facebook.com/astrazenecacareers/"><img alt="" src={facebook} id="www3"></img></a></p>
+
                             </span>
                             <span className="cards--three__rect-2">
                                 <span className="shadow-2"></span>
@@ -57,31 +58,29 @@ function Spons() {
                             <img src={Atlasian} className="img-responsive" alt="" />
                             <span className="cards--three__rect-1">
                                 <span className="shadow-1"></span>
-                                <p className="text">ATLASSIAN</p>
+                                <p className="text"><a href="https://www.atlassian.com/"><img src={www} id="www1" alt=""></img></a><a href="https://www.linkedin.com/company/atlassian/?originalSubdomain=in"><img alt="" src={linkedin} id="www2"></img></a><a href="https://www.facebook.com/Atlassian/"><img alt="" src={facebook} id="www3"></img></a></p>
                             </span>
                             <span className="cards--three__rect-2">
                                 <span className="shadow-2"></span>
                             </span>
-
                         </div>
 
                         <div className="cards cards--three" id="photo">
                             <img src={Boeing} className="img-responsive" alt="" />
                             <span className="cards--three__rect-1">
                                 <span className="shadow-1"></span>
-                                <p className="text">BOEING</p>
+                                <p className="text"><a href="https://www.boeing.com/"><img src={www} id="www1" alt=""></img></a><a href="https://www.linkedin.com/company/boeing/"><img alt="" src={linkedin} id="www2"></img></a><a href="https://www.facebook.com/Boeing/"><img alt="" src={facebook} id="www3"></img></a></p>
                             </span>
                             <span className="cards--three__rect-2">
                                 <span className="shadow-2"></span>
                             </span>
-
                         </div>
 
                         <div className="cards cards--three" id="photo">
-                            <img src={catl} className="img-responsive" alt="" />
+                            <img src={CAT} className="img-responsive" alt="" />
                             <span className="cards--three__rect-1">
                                 <span className="shadow-1"></span>
-                                <p className="text">CATERPILLAR</p>
+                                <p className="text"><a href="https://www.caterpillar.com/"><img src={www} id="www1" alt=""></img></a><a href="https://www.linkedin.com/company/caterpillar-inc/"><img alt="" src={linkedin} id="www2"></img></a><a href="https://www.facebook.com/caterpillarinc/"><img alt="" src={facebook} id="www3"></img></a></p>
                             </span>
                             <span className="cards--three__rect-2">
                                 <span className="shadow-2"></span>
@@ -93,7 +92,7 @@ function Spons() {
                             <img src={KLA} className="img-responsive" alt="" />
                             <span className="cards--three__rect-1">
                                 <span className="shadow-1"></span>
-                                <p className="text">KLA</p>
+                                <p className="text"><a href="https://www.kla-tencor.com/company"><img src={www} id="www1" alt=""></img></a><a href="https://www.linkedin.com/company/klacorp/"><img alt="" src={linkedin} id="www2"></img></a><a href="https://www.facebook.com/KLAcorp/"><img alt="" src={facebook} id="www3"></img></a></p>
                             </span>
                             <span className="cards--three__rect-2">
                                 <span className="shadow-2"></span>
@@ -105,7 +104,7 @@ function Spons() {
                             <img src={Mahindra} className="img-responsive" alt="" />
                             <span className="cards--three__rect-1">
                                 <span className="shadow-1"></span>
-                                <p className="text">MAHINDRA Rise</p>
+                                <p className="text"><a href="https://www.mahindra.com/"><img src={www} id="www1" alt=""></img></a><a href="https://www.linkedin.com/company/mahindragroup/?originalSubdomain=in"><img alt="" src={linkedin} id="www2"></img></a><a href="https://www.facebook.com/MahindraRise/"><img alt="" src={facebook} id="www3"></img></a></p>
                             </span>
                             <span className="cards--three__rect-2">
                                 <span className="shadow-2"></span>
@@ -117,7 +116,8 @@ function Spons() {
                             <img src={Salesforces} className="img-responsive" alt="" />
                             <span className="cards--three__rect-1">
                                 <span className="shadow-1"></span>
-                                <p className="text">Salesforce</p>
+                                <p className="text"><a href="https://www.salesforce.com/in/"><img src={www} id="www1" alt=""></img></a><a href="https://www.linkedin.com/company/salesforce/"><img alt="" src={linkedin} id="www2"></img></a><a href="https://www.facebook.com/salesforce/"><img alt="" src={facebook} id="www3"></img></a></p>
+
                             </span>
                             <span className="cards--three__rect-2">
                                 <span className="shadow-2"></span>
@@ -128,7 +128,8 @@ function Spons() {
                             <img src={foxmula} className="img-responsive" alt="" />
                             <span className="cards--three__rect-1">
                                 <span className="shadow-1"></span>
-                                <p className="text">FOXMULA</p>
+                                <p className="text"><a href="https://foxmula.com/"><img src={www} id="www1" alt=""></img></a><a href="https://www.linkedin.com/company/foxmula-official/?originalSubdomain=in"><img alt="" src={linkedin} id="www2"></img></a><a href="https://www.facebook.com/foxmulaOfficial/"><img alt="" src={facebook} id="www3"></img></a></p>
+
                             </span>
                             <span className="cards--three__rect-2">
                                 <span className="shadow-2"></span>
@@ -140,7 +141,8 @@ function Spons() {
                             <img src={innovaccer} className="img-responsive" alt="" />
                             <span className="cards--three__rect-1">
                                 <span className="shadow-1"></span>
-                                <p className="text">INNOVACCER</p>
+                                <p className="text"><a href="https://innovaccer.com/"><img src={www} id="www1" alt=""></img></a><a href="https://www.linkedin.com/company/innovaccer/?originalSubdomain=in"><img alt="" src={linkedin} id="www2"></img></a><a href="https://www.facebook.com/InnovAccer/"><img alt="" src={facebook} id="www3"></img></a></p>
+
                             </span>
                             <span className="cards--three__rect-2">
                                 <span className="shadow-2"></span>
@@ -152,7 +154,8 @@ function Spons() {
                             <img src={kamal} className="img-responsive" alt="" />
                             <span className="cards--three__rect-1">
                                 <span className="shadow-1"></span>
-                                <p className="text">KAMAL wears</p>
+                                <p className="text"><a href="https://www.kla-tencor.com/company"><img src={www} id="www1" alt=""></img></a><a href="https://www.linkedin.com/company/klacorp/"><img alt="" src={linkedin} id="www2"></img></a><a href="https://www.facebook.com/KLAcorp/"><img alt="" src={facebook} id="www3"></img></a></p>
+
                             </span>
                             <span className="cards--three__rect-2">
                                 <span className="shadow-2"></span>
@@ -164,7 +167,8 @@ function Spons() {
                             <img src={LT} className="img-responsive" alt="" />
                             <span className="cards--three__rect-1">
                                 <span className="shadow-1"></span>
-                                <p className="text">L&T Construction</p>
+                                <p className="text"><a href="https://www.larsentoubro.com/"><img src={www} id="www1" alt=""></img></a><a href="https://www.linkedin.com/company/larsen-&-toubro-limited/?originalSubdomain=in"><img alt="" src={linkedin} id="www2"></img></a><a href="https://www.facebook.com/LT-Construction-364964870240516/"><img alt="" src={facebook} id="www3"></img></a></p>
+
                             </span>
                             <span className="cards--three__rect-2">
                                 <span className="shadow-2"></span>
@@ -176,7 +180,8 @@ function Spons() {
                             <img src={mtx} className="img-responsive" alt="" />
                             <span className="cards--three__rect-1">
                                 <span className="shadow-1"></span>
-                                <p className="text">MTX</p>
+                                <p className="text"><a href="https://www.mtxb2b.com/s/"><img src={www} id="www1" alt=""></img></a><a href="https://www.linkedin.com/company/mtx-group/?originalSubdomain=in"><img alt="" src={linkedin} id="www2"></img></a><a href="https://www.facebook.com/mtxb2b/"><img alt="" src={facebook} id="www3"></img></a></p>
+
                             </span>
                             <span className="cards--three__rect-2">
                                 <span className="shadow-2"></span>
@@ -188,7 +193,8 @@ function Spons() {
                             <img src={subex} className="img-responsive" alt="" />
                             <span className="cards--three__rect-1">
                                 <span className="shadow-1"></span>
-                                <p className="text">SUBEX</p>
+                                <p className="text"><a href="https://www.subex.com/"><img src={www} id="www1" alt=""></img></a><a href="https://www.linkedin.com/company/subex-ltd/?originalSubdomain=in"><img alt="" src={linkedin} id="www2"></img></a><a href="https://www.facebook.com/SubexLimited/"><img alt="" src={facebook} id="www3"></img></a></p>
+
                             </span>
                             <span className="cards--three__rect-2">
                                 <span className="shadow-2"></span>
@@ -197,10 +203,11 @@ function Spons() {
                         </div>
 
                         <div className="cards cards--three" id="photo">
-                            <img src={surana} className="img-responsive" alt="" id="surana"/>
+                            <img src={surana} className="img-responsive" alt="" id="surana" />
                             <span className="cards--three__rect-1">
                                 <span className="shadow-1"></span>
-                                <p className="text">SURANA & SURANA</p>
+                                <p className="text"><a href="https://suranaandsurana.com/"><img src={www} id="www1" alt=""></img></a><a href="https://www.linkedin.com/company/surana-&-surana-internartional-attorneys/?originalSubdomain=in"><img alt="" src={linkedin} id="www2"></img></a><a href="https://www.facebook.com/Surana-Surana-International-Attorneys-125263729279918/"><img alt="" src={facebook} id="www3"></img></a></p>
+
                             </span>
                             <span className="cards--three__rect-2">
                                 <span className="shadow-2"></span>
@@ -212,7 +219,7 @@ function Spons() {
                             <img src={Wells} className="img-responsive" alt="" />
                             <span className="cards--three__rect-1">
                                 <span className="shadow-1"></span>
-                                <p className="text">WELLS FARGO</p>
+                                <p className="text"><a href="https://www.wellsfargo.com/"><img src={www} id="www1" alt=""></img></a><a href="https://www.linkedin.com/company/wellsfargo/"><img alt="" src={linkedin} id="www2"></img></a><a href="https://www.facebook.com/wellsfargo/"><img alt="" src={facebook} id="www3"></img></a></p>
                             </span>
                             <span className="cards--three__rect-2">
                                 <span className="shadow-2"></span>
@@ -220,31 +227,25 @@ function Spons() {
                         </div>
 
                     </div>
-                        </div>
-                    <br />
-                    <br />
+                </div>
+                <br />
+                <br />
 
-                    <div className="tooltip">
+                <div className="tooltip">
 
-                        <button id="flip" className="fill">Approach Us</button>
-                        <span className="tooltiptext"><a href="mailto:sponsorship@shaastra.org">sponsorship@shaastra.org</a><br />Nithish Venkatesh - 7200939339<br />Anand Logesh RR - 7358232804</span>
-
-                        {/* <div>
-                        {/* <h2>Triangle</h2> 
-                        <button id="flip" className="fill tooltip--triangle" data-tooltip="Hello World!">Approach Us</button>
-                        <span className="tooltip"></span>
-                    </div> */}
-                    </div>
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <p id="about">About Us</p><br /><br />
-                    <p className="boxx">Shaastra, ever since it's conception has been growing by leaps and bounds and this wouldn't have been achieved without the support of the student community and our corporate partners. We are continuously on the look out for long-term and mutually beneficial associations with reputed organizations from around the globe. In case your organization shares and believes in Shaastra’s vision, we would love to work out an association. We always welcome ideas, queries and criticism of any kind and hence, do feel free to contact us.</p>
-                    <br />
-                    <br />
+                    <button id="flip" className="fill">Approach Us</button>
+                    <span className="tooltiptext"><a href="mailto:sponsorship@shaastra.org">sponsorship@shaastra.org</a><br />Nithish Venkatesh - 7200939339<br />Anand Logesh RR - 7358232804</span>
+                </div>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <p id="about">About Us</p><br /><br />
+                <p className="boxx">Shaastra, ever since it's conception has been growing by leaps and bounds and this wouldn't have been achieved without the support of the student community and our corporate partners. We are continuously on the look out for long-term and mutually beneficial associations with reputed organizations from around the globe. In case your organization shares and believes in Shaastra’s vision, we would love to work out an association. We always welcome ideas, queries and criticism of any kind and hence, do feel free to contact us.</p>
+                <br />
+                <br />
                 <Footer />
             </CustomBox>
         </>
