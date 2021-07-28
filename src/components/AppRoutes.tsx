80623504d596a711d33a7a53d4c95833a43205ab
Prithivi-Raj <na19b052@smail.iitm.ac.in>
2021-07-28 14:13:31 +0530

    fin

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -1,6 +1,9 @@
 import * as React from "react"
 import {BrowserRouter as Router, Route} from "react-router-dom"
 import Home from "./pages/Home"
+import Envisage from "./pages/Envisage"
+import Shows from "./pages/Shows"
+import Project from "./pages/Project";
 
 interface Props {
     
@@ -10,6 +13,9 @@ const AppRoutes = (props: Props) => {
     return (
         <Router>
             <Route exact path="/" component={Home} />
+            <Route path="/envisage" component={Envisage} />
+            <Route path="/project/:id" component={Project} />
+            <Route path="/shows" component={Shows} />
         </Router>
     )
 }
