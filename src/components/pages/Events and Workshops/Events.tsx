9e166789d34e0e4d0d8110a3c95964da16e56b9d
Drishtipeshwani <archiepeshwani09@gmail.com>
2021-09-14 15:49:04 +0530

    Added Footer for Event and Workshop pages

--- src/components/pages/Events and Workshops/Events.tsx
+++ src/components/pages/Events and Workshops/Events.tsx
@@ -1,43 +1,49 @@
 import React from 'react'
 import {
-    Text,
-    Stack,
-    Flex,
-    Center,
-    Box,
-    Heading,
-    Image,
-    SimpleGrid,
-    Container
+  Text,
+  Stack,
+  Flex,
+  Center,
+  Box,
+  Heading,
+  Image,
+  SimpleGrid,
+  Container,
 } from '@chakra-ui/react'
 import { useColorModeValue } from '@chakra-ui/color-mode'
 import CustomBox from '../../shared/CustomBox'
+import Footer from '../../shared/Footer'
 
 function Events() {
-    return (
-        <CustomBox>
-          <Box h='100vh'>
+  return (
+    <CustomBox>
+      <Box h='100vh'>
         <Stack
-        textAlign={'center'}
-        align={'center'}
-        spacing={{ base: 8, md: 8}}
-        py={{ base: 20, md: 20 }}>
-        <Heading
-          fontWeight={600}
-          fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
-          lineHeight={'110%'}>
-          Explore{' '}
-          <Text as={'span'} color={'orange.400'}>
-            Events
+          textAlign={'center'}
+          align={'center'}
+          spacing={{ base: 8, md: 8 }}
+          py={{ base: 20, md: 20 }}
+        >
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
+            lineHeight={'110%'}
+          >
+            Explore{' '}
+            <Text as={'span'} color={'orange.400'}>
+              Events
+            </Text>
+          </Heading>
+          <Text color={'gray.500'} maxW={'3xl'}>
+            Have to add Events Content
           </Text>
-        </Heading>
-        <Text color={'gray.500'} maxW={'3xl'}>
-          Have to add Events Content
-         </Text>
-         </Stack>
-        </Box>
-        </CustomBox>
-    )
+        </Stack>
+      </Box>
+      <Footer
+        designed={[{ name: 'Drishti', mail: 'me19b100@smail.iitm.ac.in' }]}
+      />
+    </CustomBox>
+  )
 }
 
 export default Events
