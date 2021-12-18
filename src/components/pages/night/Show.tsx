b302b7e6b42e5d5b3399ed2cf933d43a0f36f4ce
mitesh-200228 <81156492+mitesh-200228@users.noreply.github.com>
2021-12-18 14:32:47 +0530

    Shows done , Sponsors Route Fixed

--- src/components/pages/night/Show.tsx
+++ src/components/pages/night/Show.tsx
@@ -2,147 +2,174 @@ import React, { useEffect } from "react"
 import CustomBox from "./../../shared/CustomBox";
 import Header from "./../../shared/Header"
 import Footer from "../../shared/Footer";
-import { Heading, Flex, Box, Image, useColorModeValue } from "@chakra-ui/react";
-import w from "./../../../images/night/shows_1.png";
-import q from "./../../../images/night/shows_2.png";
-import r from "./../../../images/night/shows_3.png";
-import Bg from "./../../../images/night/shows-bg.jpg"
+import { Heading, Flex, Box, Image, Text, useColorModeValue } from "@chakra-ui/react";
+import shows from "./../../../images/shows.jpg"
 import event from "./script";
-import "../../../styles/night/show.css"
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-
-export default function Show()
-{
-    const bg = useColorModeValue("light" , "dark")
-
+import "../../../styles/show.css"
+import {
+    VerticalTimeline,
+    VerticalTimelineElement
+} from "react-vertical-timeline-component";
+// import "../../../styles/timeline.css"
+import "react-vertical-timeline-component/style.min.css";
+// import "../../../styles/letters.css"
+import Timeline from 'react-timeline-semantic-ui';
+export default function Show() {
+    const bg = useColorModeValue("light", "dark")
+    let x = 1;
+    if (window.innerWidth < 920) x = 0;
     useEffect(
         () => {
-            var headL:HTMLElement = document.getElementById("show-head-l")!
-        // var headR: HTMLElement = document.getElementById("show-head-r")!
-        headL.style.animation = "left 1.5s linear";
-        // headR.style.animation = "left 2s ease"
+            var headL: HTMLElement = document.getElementById("show-head-l")!
+            // var headR: HTMLElement = document.getElementById("show-head-r")!
+            headL.style.animation = "left 1.5s linear";
+            // headR.style.animation = "left 2s ease"
+            var items = document.querySelectorAll(".timeline li");
+            var items = document.querySelectorAll(".timeline li");
+
+            function isElementInViewport(el) {
+                var rect = el.getBoundingClientRect();
+                return (
+                    rect.top >= 0 &&
+                    rect.left >= 0 &&
+                    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+                );
+            }
+
+            function callbackFunc() {
+                for (var i = 0; i < items.length; i++) {
+                    if (isElementInViewport(items[i])) {
+                        if (!items[i].classList.contains("in-view")) {
+                            items[i].classList.add("in-view");
+                        }
+                    } else if (items[i].classList.contains("in-view")) {
+                        items[i].classList.remove("in-view");
+                    }
+                }
+            }
+
+            window.addEventListener("load", callbackFunc);
+            window.addEventListener("scroll", callbackFunc);
+
         }
     )
 
-    return(
+    return (
         <>
             <CustomBox>
-                <Box className="shows-main" backgroundColor="transparent" zIndex="2">
-                <Header></Header>
-                <Flex width="100vw" className="shows"
-                    justifyContent="space-between" marginBottom="10vh"
-                    backgroundColor="transparent" paddingTop="10vh" alignItems="center"
-                >
-                    <Box width="20%" height="3vh" className={` ${bg}`} id="show-head-l"> </Box>
-                    <Heading backgroundColor="transparent" fontSize="4vw">SHOWS</Heading>
-                    <Box width="20%" height="3vh"className={`${bg}`} id="show-head-r" > </Box>
-                </Flex>
-                <Box margin="auto"
-                    width="80vw" fontSize="2xl" fontWeight="600"
-                    textAlign="center" marginBottom="15vh"   
-                    borderRadius="12px" 
-                    className={`shows-info ${bg}`}
-                    textColor="#0e101b" padding="8vh"
-                    backdropBlur={"30px"}
-                >
-                    The flagship event of Shaastra, <b>Shaastra Nights</b>, has made the former a noteworthy leader amongst parallel 
-                    fests in the country. Shaastra Nights provide a platform for exquisite performers to display their artistic
-                     prowess while giving the audience a once-in-a-lifetime opportunity to witness a confluence of technology 
-                     and culture. Shaastra Nights is non-ticketed - an attribute that distinguishes it from shows akin; it 
-                     enables students and the public to access quality entertainment at its finest. Behind the warranted glitz 
-                     and glamour, Shaastra Nights' true intentions shine - <b>diversity</b> and <b>inclusivity</b>. 
-                     <br /> <br />
-                     Professional shows are a fitting end to each day of Shaastra. These <b>techno-entertainment shows</b>, ever 
-                     since their conception, have hosted artists from across the globe. Our audience counts up in thousands,
-                      which is a testament to its success and popularity. These include Mirage, a laser show by an 
-                      international group, Sand Art show by world-renowned artists, Bike Stunt show by French performers 
-                      and EDM (Electronic Dance Music) Night.
+                <Box backgroundColor="transparent" zIndex="2">
+                    <Header></Header>
+                    <Flex width="100vw" className="shows"
+                        justifyContent="space-between" marginBottom="10vh"
+                        backgroundColor="transparent" paddingTop="10vh" alignItems="center"
+                    >
+                        <Box width="20%" height="3vh" className={` ${bg}`} id="show-head-l"> </Box>
+                        <Heading backgroundColor="transparent" fontSize="7xl">SHOWS</Heading>
+                        <Box width="20%" height="3vh" className={`${bg}`} id="show-head-r" > </Box>
+                    </Flex>
+                    <Flex width="100%" justifyContent="space-around" paddingLeft="20px">
+                        {x ? (
+                            <>
+                                <Flex width="60%" textAlign="start" fontSize="18px" fontWeight="600">
+                                    The flagship event of Shaastra, Shaastra Nights, has made the former a noteworthy leader amongst parallel fests in the country. Shaastra Nights provide a platform for exquisite performers to display their artistic prowess while giving the audience a once-in-a-lifetime opportunity to witness a confluence of technology and culture. Shaastra Nights is non-ticketed - an attribute that distinguishes it from shows akin; it enables students and the public to access quality entertainment at its finest. Behind the warranted glitz and glamour, Shaastra Nights' true intentions shine - diversity and inclusivity.
+                                    <br />
+                                    <br />
+                                    Professional shows are a fitting end to each day of Shaastra. These techno-entertainment shows, ever since their conception, have hosted artists from across the globe. Our audience counts up in thousands, which is a testament to its success and popularity. These include Mirage, a laser show by an international group, Sand Art show by world-renowned artists, Bike Stunt show by French performers and EDM (Electronic Dance Music) Night.
+                                    <br />
+                                    <br />
+                                </Flex>
+                                <Flex backgroundImage={shows} width="380px" backgroundSize="cover" borderRadius="50%" alt="shows">
+
+                                </Flex>
+                            </>
+                        ) : (
+                            <>
+                                <Flex width="80%" textAlign="center" fontSize="18px" fontWeight="600" paddingBottom="20px">
+                                    The flagship event of Shaastra, Shaastra Nights, has made the former a noteworthy leader amongst parallel fests in the country. Shaastra Nights provide a platform for exquisite performers to display their artistic prowess while giving the audience a once-in-a-lifetime opportunity to witness a confluence of technology and culture. Shaastra Nights is non-ticketed - an attribute that distinguishes it from shows akin; it enables students and the public to access quality entertainment at its finest. Behind the warranted glitz and glamour, Shaastra Nights' true intentions shine - diversity and inclusivity.
+                                    <br />
+                                    <br />
+                                    Professional shows are a fitting end to each day of Shaastra. These techno-entertainment shows, ever since their conception, have hosted artists from across the globe. Our audience counts up in thousands, which is a testament to its success and popularity. These include Mirage, a laser show by an international group, Sand Art show by world-renowned artists, Bike Stunt show by French performers and EDM (Electronic Dance Music) Night.
+                                    <br />
+                                    <br />
+                                </Flex>
+                            </>
+                        )}
+                    </Flex>
+                    <Flex width="60vw" margin="auto" justifyContent="center" paddingLeft="10px" fontSize="5xl" fontWeight="600" className="shows-image">
+                        <Text>
+                            TIMELINE
+                        </Text>
+                    </Flex>
+                    <section className="timeline">
+                        <ul>
+                            <li>
+                                <div>
+                                    <time>2016</time>
+                                    <div className="discovery">
+                                        <h1>Adelaida</h1>
+                                        <h1>Vivek Patil</h1>
+                                        <h1>Freestyle Air - 3 acts</h1>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <time>2017</time>
+                                    <div className="discovery">
+                                        <h1>IDRL drone race</h1>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <time>2018</time>
+                                    <div className="discovery">
+                                        <h1>Mariana Bo</h1>
+                                        <h1>Get Massive</h1>
+                                        <h1>Broken Symmetry</h1>
+                                        <h1>Suhani Shah</h1>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <time>2019</time>
+                                    <div className="discovery">
+                                        <h1>Vivek Patil</h1>
+                                        <h1>Vilas Nayak</h1>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <time>2020</time>
+                                    <div className="discovery">
+                                        <h1>Heimdall</h1>
+                                        <h1>Vivek Desai</h1>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="discovery1">
+                                    <time>2021</time>
+                                    <div className="discovery" id="last">
+                                        <h1>Kenny Sebastian</h1>
+                                        <h1>Soda (Sudarshan Ramamurthy)</h1>
+                                        <h1>Ray Li</h1>
+                                        <h1>Dan Rhodes</h1>
+                                        <h1>Sonic Snares</h1>
+                                        <h1>Sushil Jazz</h1>
+                                        <h1>Rishabh Rajan</h1>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </section>
+                    <Box backgroundColor="#0e101bc9" marginTop="50px">
+                        <Footer designed={[{ name: "Mitesh", mail: "ce20b025@smail.iitm.ac.in" }]} />
+                    </Box>
                 </Box>
-                <Flex width="60vw" margin="auto" justifyContent="space-between" className="shows-image">
-                    <Image src={w} boxSize="18vw" borderRadius="50%" border="5px solid white"></Image>
-                    <Image src={q} boxSize="18vw" borderRadius="50%" border="5px solid white"></Image>
-                    <Image src={r} boxSize="18vw" borderRadius="50%" border="5px solid white"></Image>
-                </Flex>
-                <VerticalTimeline>
-                <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                contentStyle={{ background: '#0e101b', color: '#fff' }}
-                contentArrowStyle={{ borderRight: '7px solid  #0e101b' }}
-                iconStyle={{ background: '#0e101bc9', color: '#fee9e1' }}>
-                    <Flex flexDirection="column" >
-                        <h3>2016</h3>
-                        <p>Adelaida</p>
-                        <p>Vivek Patil</p>
-                        <p>Freestyle Air - 3 acts</p>
-                    </Flex>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                contentStyle={{ background: '#0e101b', color: '#fff' }}
-                contentArrowStyle={{ borderRight: '7px solid  #0e101b' }}
-                iconStyle={{ background: '#0e101bc9', color: '#fff' }}>
-                    <Flex flexDirection="column">
-                        <h3>2017</h3>
-                        <p>IDRL drone race </p>
-                    </Flex>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                contentStyle={{ background: '#0e101b', color: '#fff' }}
-                contentArrowStyle={{ borderRight: '7px solid  #0e101b' }}
-                iconStyle={{ background: '#0e101bc9', color: '#fff' }}>
-                    <Flex flexDirection="column">
-                        <h3>2018</h3>
-                        <p>Mariana Bo</p>
-                        <p>Get Massive</p>
-                        <p>Broken Symmetry</p>
-                        <p>Suhani Shah</p>
-                    </Flex>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                contentStyle={{ background: '#0e101b', color: '#fff' }}
-                contentArrowStyle={{ borderRight: '7px solid  #0e101b' }}
-                iconStyle={{ background: '#0e101bc9', color: '#fff' }}>
-                    <Flex flexDirection="column">
-                        <h3>2019</h3>
-                        <p>Vivek Patil</p>
-                        <p>Vilas Nayak</p>
-                    </Flex>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                contentStyle={{ background: '#0e101b', color: '#fff' }}
-                contentArrowStyle={{ borderRight: '7px solid #0e101b' }}
-                iconStyle={{ background: '#0e101bc9', color: '#fff' }}>
-                    <Flex flexDirection="column">
-                        <h3>2020</h3>
-                        <p>Heimdall </p>
-                        <p>Vivek Desai</p>
-                    </Flex>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                contentStyle={{ background: '#0e101b', color: '#fff' }}
-                contentArrowStyle={{ borderRight: '7px solid  #0e101b' }}
-                iconStyle={{ background: '#0e101bc9', color: '#fff' }}>
-                    <Flex flexDirection="column">
-                        <h3>2021</h3>
-                        <p>Kenny Sebastian</p>
-                        <p>Soda (Sudarshan Ramamurthy)</p>
-                        <p>Ray Li</p>
-                        <p>Dan Rhodes</p>
-                        <p>Sonic Snares</p>
-                        <p>Sushil Jazz</p>
-                        <p>Rishabh Rajan</p>
-                    </Flex>
-                </VerticalTimelineElement>
-                </VerticalTimeline>
-           <Box backgroundColor="#0e101bc9">
-           <Footer designed={[{name : "Krithikaa",mail :"be20b020@smail.iitm.ac.in"}]} />
-           </Box>
-            </Box>
             </CustomBox>
         </>
     )
