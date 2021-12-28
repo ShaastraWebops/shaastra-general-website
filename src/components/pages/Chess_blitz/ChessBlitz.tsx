024a8a70bb23e503d8db66e2ecebc0485f78108e
JanithMS <janithms9920@gmail.com>
2021-12-28 12:26:29 +0530

    added payment for chess

--- src/components/pages/Chess_blitz/ChessBlitz.tsx
+++ src/components/pages/Chess_blitz/ChessBlitz.tsx
@@ -3,73 +3,201 @@ import "../../../styles/Chess.css";
 import Footer from "../../shared/Footer";
 import CustomBox from "../../shared/CustomBox";
 import { useColorModeValue } from "@chakra-ui/color-mode";
-import { useGetChessDetailsCsvQuery, useRegisterChessMutation } from "../../../generated/graphql";
-import { Modal, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react";
+import {
+  RegisterChessMutation,
+  useCapturePaymentChessMutation,
+  useGetChessDetailsCsvQuery,
+  useRegisterChessMutation,
+} from "../../../generated/graphql";
+import {
+  Modal,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 import fileDownload from "js-file-download";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+function loadScript(src: string) {
+  return new Promise((resolve) => {
+    const script = document.createElement("script");
+    script.src = src;
+    script.onload = () => {
+      resolve(true);
+    };
+    script.onerror = () => {
+      resolve(false);
+    };
+    document.body.appendChild(script);
+  });
+}
 
 function ChessBlitz() {
   const theme = useColorModeValue("white", "black");
-  const [username , setUsername] = useState('');
-  const [rating , setRating] = useState('');
-  const [title , setTitle] = useState('');
+  const [username, setUsername] = useState("");
+  const [rating, setRating] = useState("");
+  const [title, setTitle] = useState("");
   var { isOpen, onOpen, onClose } = useDisclosure();
 
-  const [regitserChess , {data,loading , error}] = useRegisterChessMutation();
-  const {data : data1} = useGetChessDetailsCsvQuery();
-  const [isAdmin , setIsAdmin] = useState(false)
-  useEffect(()=>{
-    if(localStorage.getItem('role') === "Admin"){
-      setIsAdmin(true)
-    }
-  },[])
-  const handleChessRegister = async () =>{
-
-    await regitserChess({
-      variables :{
-        data :{
-          username,
-          rating,
-          title
-        }
+  const [
+    regitserChess,
+    { loading: registerChessLoading, error: registerChessError },
+  ] = useRegisterChessMutation({
+    /******** On create order completion, open Razorpay ********/
+    async onCompleted(data) {
+      if (data.registerChess) {
+        await loadRazorpay(data);
       }
-    }).catch(err => console.log(err))
-  }
-  if(loading)
-  {
-      onClose = () => {}
-      return(
-          <Modal isOpen={true} onClose={onClose}>
-              <ModalOverlay />
-              <ModalContent backgroundColor="#e2e19c" color="black">
-                  <ModalHeader>Loading...</ModalHeader>
-                  <ModalCloseButton />
-               </ModalContent>
-          </Modal>
-      )
-  } else if(error)
-  {
-      onClose = () => {window.location.reload()}
-              return(
-                  <Modal isOpen={true} onClose={onClose}>
-                      <ModalOverlay />
-                      <ModalContent backgroundColor="#f1aaaa" color="black">
-                          <ModalHeader>{error.message}</ModalHeader>
-                          <ModalCloseButton />
-                      </ModalContent>
-                  </Modal>
-              )
-  }else if(data?.registerChess)
-  {
-      onClose = () => {window.location.reload()}
-              return(
-                  <Modal isOpen={true} onClose={onClose}>
-                      <ModalOverlay />
-                      <ModalContent backgroundColor="#A7EAAA" color="black">
-                          <ModalHeader>Registration Successful</ModalHeader>
-                          <ModalCloseButton />
-                      </ModalContent>
-                  </Modal>
-              )
+    },
+  });
+
+  const [
+    capturePaymentChessMutation,
+    {
+      data: capturePaymentData,
+      loading: capturePaymentLoading,
+      error: capturePaymentError,
+    },
+  ] = useCapturePaymentChessMutation();
+
+  const { data: data1 } = useGetChessDetailsCsvQuery();
+  const [isAdmin, setIsAdmin] = useState(false);
+  useEffect(() => {
+    if (localStorage.getItem("role") === "Admin") {
+      setIsAdmin(true);
+    }
+  }, []);
+  const handleChessRegister = async () => {
+    try {
+      await regitserChess({
+        variables: {
+          data: {
+            username,
+            rating,
+            title,
+          },
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+    //  catch((err) => console.log(err));
+  };
+
+  /******** Callback Functions ********/
+  const loadRazorpay = async (data: RegisterChessMutation) => {
+    /******** Load Razorpay Script ********/
+    const res = await loadScript(
+      "https://checkout.razorpay.com/v1/checkout.js"
+    );
+    if (!res) {
+      alert("Razorpay SDK failed to load");
+      return;
+    }
+
+    /******** Razorpay Options ********/
+    const options = {
+      key: process.env.REACT_APP_RAZORPAY_KEY,
+      amount: "20000",
+      currency: "INR",
+      name: "BlitzChess",
+      image: "", //TODO: Add the shaastra link here
+      order_id: data.registerChess.orderId,
+
+      /******** Handler function to update the payment details ********/
+      handler: async function (response: any) {
+        try {
+          await capturePaymentChessMutation({
+            variables: {
+              input: {
+                orderId: response.razorpay_order_id,
+                payementId: response.razorpay_payment_id,
+                paymentSignature: response.razorpay_signature,
+              },
+            },
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      },
+      theme: {
+        color: "#3399cc",
+      },
+    };
+
+    /******** Open Razorpay ********/
+    const rzp1 = new (window as any).Razorpay(options);
+    rzp1.open();
+  };
+  if (registerChessLoading) {
+    onClose = () => {};
+    return (
+      <Modal isOpen={true} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent backgroundColor="#e2e19c" color="black">
+          <ModalHeader>Loading...</ModalHeader>
+          <ModalCloseButton />
+        </ModalContent>
+      </Modal>
+    );
+  } else if (capturePaymentLoading) {
+    onClose = () => {};
+    return (
+      <Modal isOpen={true} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent backgroundColor="#e2e19c" color="black">
+          <ModalHeader>
+            Don't close or reload the site, until you get the payment
+            confirmation. Loading...
+          </ModalHeader>
+          <ModalCloseButton />
+        </ModalContent>
+      </Modal>
+    );
+  } else if (registerChessError) {
+    console.log(registerChessError);
+    onClose = () => {
+      window.location.reload();
+    };
+    return (
+      <Modal isOpen={true} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent backgroundColor="#f1aaaa" color="black">
+          <ModalHeader>{registerChessError.message}</ModalHeader>
+          <ModalCloseButton />
+        </ModalContent>
+      </Modal>
+    );
+  } else if (capturePaymentError) {
+    onClose = () => {
+      window.location.reload();
+    };
+    return (
+      <Modal isOpen={true} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent backgroundColor="#f1aaaa" color="black">
+          <ModalHeader>Payment Failed</ModalHeader>
+          <ModalCloseButton />
+        </ModalContent>
+      </Modal>
+    );
+  } else if (capturePaymentData?.capturePaymentChess) {
+    onClose = () => {
+      window.location.reload();
+    };
+    return (
+      <Modal isOpen={true} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent backgroundColor="#A7EAAA" color="black">
+          <ModalHeader>Registration Successful</ModalHeader>
+          <ModalCloseButton />
+        </ModalContent>
+      </Modal>
+    );
   }
 
   return (
@@ -88,18 +216,42 @@ function ChessBlitz() {
               pool of INR 85000, as well as various prizes. Don't miss out on
               your chance to play in the year's largest online blitz tournament!
             </p>
-            <form style={{'color' : "black", 'margin' : '10px'}}>
-              <input type={'text'} style={{'margin': "5px"}} value={username} onChange={(e)=> setUsername(e.target.value)} placeholder={'Username'}/>
-              <input type={'text'} style={{'margin': "5px"}} value={rating} onChange={(e)=> setRating(e.target.value)} placeholder={'Rating'}/>
-              <input type={'text'} style={{'margin': "5px"}} value={title} onChange={(e)=> setTitle(e.target.value)}  placeholder={'title'}/>
+            <form style={{ color: "black", margin: "10px" }}>
+              <input
+                type={"text"}
+                style={{ margin: "5px" }}
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder={"Username"}
+              />
+              <input
+                type={"text"}
+                style={{ margin: "5px" }}
+                value={rating}
+                onChange={(e) => setRating(e.target.value)}
+                placeholder={"Rating"}
+              />
+              <input
+                type={"text"}
+                style={{ margin: "5px" }}
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder={"title"}
+              />
             </form>
             <button onClick={handleChessRegister}>Register now</button>
-           {
-             isAdmin === true ?  <button  onClick={() => {
-              fileDownload(data1?.getChessDetailsCSV!, `BlitzChess_regristants.csv`);
-            }}
-            >Download Users Data CSV</button> : null
-           }
+            {isAdmin === true ? (
+              <button
+                onClick={() => {
+                  fileDownload(
+                    data1?.getChessDetailsCSV!,
+                    `BlitzChess_regristants.csv`
+                  );
+                }}
+              >
+                Download Users Data CSV
+              </button>
+            ) : null}
           </div>
         </div>
         <div className="ChessBlitz_Overview">
