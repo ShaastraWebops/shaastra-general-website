a32b86727a228d0ae10dc595c741f3c9d4017b9b
krithikaa22 <kuruthiga22@gmail.com>
2022-01-08 14:39:54 +0530

    contact us updated

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -39,13 +39,14 @@ import {
     ButtonGroup,
   } from '@chakra-ui/react'
 
+import "../../styles/App.css"
 import ContactBg from "../../images/ContactBg.png"
 
 const Contact = () => {
     return(
         <CustomBox>
-            <Flex width="100vw" padding={["20vw 0 0 0","4vw 0 0 0"]} justifyContent={"space-between"} flexDirection={["column", "row"]} alignItems={"center"}>
-                <Flex paddingLeft={"2vw"} flexDirection={"column"} width="50%" alignItems={"center"}>
+            <Flex className="contact-main" overflowX={"hidden"} width="100vw" padding={["20vw 0 0 0","4vw 0 0 0"]} justifyContent={"space-between"} flexDirection={["column", "row"]} alignItems={"center"}>
+                <Flex overflowX={"hidden"} paddingLeft={"2vw"} flexDirection={"column"} width="50%" alignItems={"center"}>
                     <Heading fontSize={["6vw","3vw"]} marginBottom={"4vh"}>CONTACT US</Heading>
 
                     {/* english */}
@@ -54,12 +55,12 @@ const Contact = () => {
                         <Text color="#A0AEC0" fontWeight={"500"} fontSize={["6vw","2vw"]}>English</Text>
                         <Flex width={["90vw","50vw"]} justifyContent={"space-evenly"}>
                             <Flex flexDirection={"column"}>
-                                <Text fontWeight={"bold"} fontSize={["4vw","1.5vw"]}>Siddhivinayak</Text>
-                                <Text fontSize={["4vw","1.5vw"]}> <a href={"http://wa.me/+919672025542"} target="_blank"><p>+91 96720 25542</p></a></Text>
+                                <Text fontWeight={"bold"} fontSize={["4vw","1.5vw"]}>Poojitha</Text>
+                                <Text fontSize={["4vw","1.5vw"]}> <a href={"http://wa.me/+917019180917"} target="_blank"><p>+91 70191 80917</p></a></Text>
                             </Flex>
                             <Flex flexDirection={"column"}>
-                                <Text fontWeight={"bold"} fontSize={["4vw","1.5vw"]}>Vyshnavi</Text>
-                                <Text fontSize={["4vw","1.5vw"]}>  <a href={"http://wa.me/+918897091122"} target="_blank"><p>+91 88970 91122</p></a></Text>
+                                <Text fontWeight={"bold"} fontSize={["4vw","1.5vw"]}>Joshik</Text>
+                                <Text fontSize={["4vw","1.5vw"]}>  <a href={"http://wa.me/+918825601711"} target="_blank"><p>+91 88256 01711</p></a></Text>
                             </Flex>
                         </Flex>
                     </Flex>
@@ -80,14 +81,30 @@ const Contact = () => {
                         </Flex>
                     </Flex>
 
-                    {/*hindi and kannada */}
+                    {/* hindi */}
+
+                    <Flex width={["90vw","50vw"]} paddingBottom={"6vh"} flexDirection={"column"} alignItems={"center"} >
+                        <Text color="#A0AEC0" fontWeight={"500"} fontSize={["6vw","2vw"]}>Hindi</Text>
+                        <Flex width={["90vw","50vw"]} justifyContent={"space-evenly"}>
+                            <Flex flexDirection={"column"}>
+                                <Text fontWeight={"bold"} fontSize={["4vw","1.5vw"]}>Ankit</Text>
+                                <Text fontSize={["4vw","1.5vw"]}><a href={"http://wa.me/+917488701168"} target="_blank"><p>+91 74887 01168</p></a></Text>
+                            </Flex>
+                            <Flex flexDirection={"column"}>
+                                <Text fontWeight={"bold"} fontSize={["4vw","1.5vw"]}>Rahul</Text>
+                                <Text fontSize={["4vw","1.5vw"]}> <a href={"http://wa.me/+919602032101"} target="_blank"><p>+91 96020 32101</p></a></Text>
+                            </Flex>
+                        </Flex>
+                    </Flex>
+
+                    {/*marathi and kannada */}
 
                     <Flex width={["90vw","50vw"]} paddingBottom={"6vh"} alignItems={"center"} justifyContent={"space-evenly"}>
                         <Flex flexDirection={"column"}>
-                            <Text color="#A0AEC0" fontWeight={"500"} fontSize={["6vw","2vw"]}>Hindi</Text>
+                            <Text color="#A0AEC0" fontWeight={"500"} fontSize={["6vw","2vw"]}>Marathi</Text>
                             <Flex flexDirection={"column"}>
-                                <Text fontWeight={"bold"} fontSize={["4w","1.5vw"]}>Aditya</Text>
-                                <Text fontSize={["4vw","1.5vw"]}><a href={"http://wa.me/+918178180545"} target="_blank"><p>+91 81781 80545</p></a></Text>
+                                <Text fontWeight={"bold"} fontSize={["4vw","1.5vw"]}>Siddhivinayak</Text>
+                                <Text fontSize={["4vw","1.5vw"]}> <a href={"http://wa.me/+919672025542"} target="_blank"><p>+91 96720 25542</p></a></Text>
                             </Flex>
                         </Flex>
                         <Flex flexDirection={"column"}>
@@ -117,6 +134,23 @@ const Contact = () => {
                             </Flex>
                         </Flex>
                     </Flex>
+
+                    {/* <Flex width={["90vw","50vw"]} paddingBottom={"4vh"} alignItems={"center"} justifyContent={"space-evenly"}>
+                        <Flex flexDirection={"column"}>
+                            <Text color="#A0AEC0" fontWeight={"500"} fontSize={["6vw","2vw"]}>Marathi</Text>
+                            <Flex flexDirection={"column"}>
+                                <Text fontWeight={"bold"} fontSize={["4vw","1.5vw"]}>Siddhivinayak</Text>
+                                <Text fontSize={["4vw","1.5vw"]}> <a href={"http://wa.me/+919672025542"} target="_blank"><p>+91 96720 25542</p></a></Text>
+                            </Flex>
+                        </Flex>
+                        <Flex flexDirection={"column"}>
+                            <Text color="#A0AEC0" fontWeight={"500"} fontSize={["6vw","2vw"]}>Malayalam</Text>
+                            <Flex flexDirection={"column"}>
+                                <Text fontWeight={"bold"} fontSize={["4vw","1.5vw"]}>Anshid</Text>
+                                <Text fontSize={["4vw","1.5vw"]}> <a href={"http://wa.me/+917592976901"} target="_blank"><p>+91 75929 76901</p></a></Text>
+                            </Flex>
+                        </Flex>
+                    </Flex> */}
 
                 </Flex>
                 <Image src={ContactBg} width="50vw" objectFit={"contain"}></Image>
